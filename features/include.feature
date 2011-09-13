f91b19bcd9a1d3b2d1ff7ab060ed34b4229dc1dc
John Van Enk <vanenkj@gmail.com>
2011-09-13 00:34:45 -0400

    Fleshed out some of the steps.

--- features/include.feature
+++ features/include.feature
@@ -4,10 +4,18 @@ Feature: Include
 	As a devloper using YAML to configure an application
 	I want to be able to include the structure of one YAML file in another
 
-	Scenario Outline: Include external file
-		Given a YAML file named "main.yaml"
-		And an external file named "external.yaml"
-		When the file named "main.yaml" contains a key with the value "INCLUDE"
-		And the key "INCLUDE" has a value of "external.yaml"
-		Then the structure of "external.yaml" should be included as a sibling of the "INCLUDE" key
-		And the "INCLUDE" key should be deleted
+	Scenario: Include external file
+		Given a directory
+		And a file named "main.yml" in the directory containing:
+			"""
+			---
+				a: 0
+				INCLUDE: external.yaml
+			"""
+		And a file named "external.yml" in the directory containing:
+			"""
+			---
+				b: 1
+			"""
+		Then the result contains the data from "external.yml" in "main.yml" at the level of INCLUDE
+		And the INCLUDE key is deleted
--- features/step_definitions/include_steps.rb
+++ features/step_definitions/include_steps.rb
@@ -6,11 +6,49 @@ end
 require 'cucumber/formatter/unicode'
 
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
+
+###
+
 require 'yaml_extras'
+require 'tmpdir'
+require 'fileutils'
+require 'yaml'
+
+def read_yaml(f)
+	YAML.load(File.read(f))
+end
 
 Before do
-	@yaml_extras = YamlExtras.new
+	@yaml_extras = nil
+	@tmpdir = Dir.tmpdir + "/cuke_tempdir"
 end
 
 After do
+	if File.exists?(@tmpdir) and File.directory?(@tmpdir)
+		FileUtils.rm_rf @tmpdir
+	end
+end
+
+###
+
+Given /^a directory$/ do
+	Dir.mkdir @tmpdir
+end
+
+Given /^a file named "([^"]*)" in the directory containing:$/ do |name, string|
+	File.open("#{@tmpdir}/#{name}", 'w') do |f|
+		f.write(string)
+	end
+end
+
+When /^the file "([^"]*)" is evaluated$/ do |name|
+	@yaml_extras = YamlExtras.new(name)
+end
+
+Then /^the result contains the data from "([^"]*)" in "([^"]*)" at the level of INCLUDE$/ do |other, main|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the INCLUDE key is deleted$/ do
+  pending # express the regexp above with the code you wish you had
 end
