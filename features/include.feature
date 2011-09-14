514865bb6b27b449a7b6b454abf81dea196bdaa1
John Van Enk <vanenkj@gmail.com>
2011-09-13 23:31:13 -0400

    How DARE hard tabs sneek into my code. >:(

--- features/include.feature
+++ features/include.feature
@@ -1,21 +1,21 @@
 # language: en
 Feature: Include
-	In order to simplify configuration data
-	As a devloper using YAML to configure an application
-	I want to be able to include the structure of one YAML file in another
+  In order to simplify configuration data
+  As a devloper using YAML to configure an application
+  I want to be able to include the structure of one YAML file in another
 
-	Scenario: Include external file
-		Given a directory
-		And a file named "main.yml" in the directory containing:
-			"""
-			---
-				a: 0
-				INCLUDE: external.yaml
-			"""
-		And a file named "external.yml" in the directory containing:
-			"""
-			---
-				b: 1
-			"""
-		Then the result contains the data from "external.yml" in "main.yml" at the level of INCLUDE
-		And the INCLUDE key is deleted
+  Scenario: Include external file
+    Given a directory
+    And a file named "main.yml" in the directory containing:
+      """
+      ---
+        a: 0
+        INCLUDE: external.yaml
+      """
+    And a file named "external.yml" in the directory containing:
+      """
+      ---
+        b: 1
+      """
+    Then the result contains the data from "external.yml" in "main.yml" at the level of INCLUDE
+    And the INCLUDE key is deleted
--- features/step_definitions/include_steps.rb
+++ features/step_definitions/include_steps.rb
@@ -1,7 +1,7 @@
 begin
-	require 'rspec/expectations'
+  require 'rspec/expectations'
 rescue LoadError
-	require 'spec/expectations'
+  require 'spec/expectations'
 end
 require 'cucumber/formatter/unicode'
 
@@ -15,34 +15,34 @@ require 'fileutils'
 require 'yaml'
 
 def read_yaml(f)
-	YAML.load(File.read(f))
+  YAML.load(File.read(f))
 end
 
 Before do
-	@yaml_extras = nil
-	@tmpdir = Dir.tmpdir + "/cuke_tempdir"
+  @yaml_extras = nil
+  @tmpdir = Dir.tmpdir + "/cuke_tempdir"
 end
 
 After do
-	if File.exists?(@tmpdir) and File.directory?(@tmpdir)
-		FileUtils.rm_rf @tmpdir
-	end
+  if File.exists?(@tmpdir) and File.directory?(@tmpdir)
+    FileUtils.rm_rf @tmpdir
+  end
 end
 
 ###
 
 Given /^a directory$/ do
-	Dir.mkdir @tmpdir
+  Dir.mkdir @tmpdir
 end
 
 Given /^a file named "([^"]*)" in the directory containing:$/ do |name, string|
-	File.open("#{@tmpdir}/#{name}", 'w') do |f|
-		f.write(string)
-	end
+  File.open("#{@tmpdir}/#{name}", 'w') do |f|
+    f.write(string)
+  end
 end
 
 When /^the file "([^"]*)" is evaluated$/ do |name|
-	@yaml_extras = YamlExtras.new(name)
+  @yaml_extras = YamlExtras.new(name)
 end
 
 Then /^the result contains the data from "([^"]*)" in "([^"]*)" at the level of INCLUDE$/ do |other, main|
