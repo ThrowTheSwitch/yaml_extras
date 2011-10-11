0454a82f312e26fdf95e5a2715862b77a5bf163a
John Van Enk <vanenkj@gmail.com>
2011-10-11 13:40:20 -0400

    Include specs have been implemented. Code passes specs.

--- lib/yaml_extras.rb
+++ lib/yaml_extras.rb
@@ -32,13 +32,53 @@ class ObjectPathing
   end
 end
 
-class YamlExtras
-  attr_accessor :result, :original
+class CyclicYamlExtrasInclude < Exception
+end
 
-  def initialize(file_name)
-    @original = YAML.load(File.read(file_name))
+class YamlExtras
+  attr_accessor :result, :includes_integrated, :file_name
+
+  def initialize(file_name, parents = [])
+    @file_name = file_name
+    @parents = parents
+    @included = []
+  end
+
+  def original
+    @original ||= YAML.load(File.read(@file_name))
   end
 
   def integrate_includes
+    raise CyclicYamlExtrasInclude if @parents.include? @file_name
+
+    unless @includes_integrated
+      @includes_integrated = original.clone
+
+      ObjectPathing.find_paths_to_keys(["INCLUDE"], @includes_integrated).each do |l|
+        # path except last key which should be the 'INCLUDE' key
+        target_obj_path = l[0...-1] 
+
+        # the object containing the 'INCLUDE' key
+        target_obj = ObjectPathing.apply_path(@includes_integrated, target_obj_path)
+
+        # the path to the file to be included
+        file_path = ObjectPathing.apply_path(@includes_integrated, l)
+
+        # remove the INCLUDE key. it's not supposed to be in the final result
+        target_obj.delete "INCLUDE"
+
+        # recursively apply YamlExtras in order to deal with any includes that
+        # may exist in the included file.
+        inc_data = YamlExtras.new(file_path, @parents.clone << @file_name).integrate_includes
+
+        # loop over the fields of the included data and stash them in the
+        # object which originally contained the 'INCLUDE' key
+        inc_data.each_pair do |k, v|
+          target_obj[k] = v
+        end
+      end
+    end
+
+    @includes_integrated
   end
 end
--- spec/yaml_extras_spec.rb
+++ spec/yaml_extras_spec.rb
@@ -56,7 +56,13 @@ end
 
 describe YamlExtras do
   describe :initialize do
-    it "should record the original object" do
+    it "should record the file name" do
+      YamlExtras.new("some_file.yml").file_name.should == "some_file.yml"
+    end
+  end
+
+  describe :original do
+    it "should return the Hash representing the loaded YAML file" do
       mock(File).read("some_file.yml") {"---\na: 5\n"}
       YamlExtras.new("some_file.yml").original.should == {"a" => 5}
     end
@@ -71,7 +77,60 @@ describe YamlExtras do
       mock(File).read("some_file.yml") { input }
       mock(File).read("other.yml") { other }
 
-      ye = YamlExtras.new("some_file.yml").integrate_includes.should == expect
+      YamlExtras.new("some_file.yml").integrate_includes.should == expect
+    end
+
+    it "should handle nested INCLUDEs" do
+      first  = "---\na: 1\nINCLUDE: second.yml\n"
+      second = "---\nb: 2\nINCLUDE: third.yml\n"
+      third  = "---\nc: 3\n"
+ 
+      expect = { "a" => 1, "b" => 2, "c" => 3 }
+
+      mock(File).read("first.yml")  { first  }
+      mock(File).read("second.yml") { second }
+      mock(File).read("third.yml")  { third  }
+
+      YamlExtras.new("first.yml").integrate_includes.should == expect
+    end
+
+    it "should allow multiple inclusion of the same file" do
+      first  = ["---",
+                "  a:",
+                "    INCLUDE: second.yml",
+                "  b:",
+                "    INCLUDE: second.yml"].join("\n")
+      second = "---\nc: 100\n"
+
+      expect = { "a" => { "c" => 100 }, "b" => { "c" => 100 } }
+
+      mock(File).read("first.yml")  { first  }
+      mock(File).read("second.yml") { second }
+      mock(File).read("second.yml") { second }
+
+      YamlExtras.new("first.yml").integrate_includes.should == expect
+    end
+
+    it "should reject direct cyclic INCLUDEs" do
+      input = "---\na: 1\nINCLUDE: some_file.yml\n"
+
+      mock(File).read("some_file.yml") { input }
+
+      lambda {
+          YamlExtras.new("some_file.yml").integrate_includes
+        }.should raise_error(CyclicYamlExtrasInclude)
+    end
+    
+    it "should reject indirect cyclic INCLUDEs" do
+      first  = "---\na: 1\nINCLUDE: second.yml\n"
+      second = "---\nb: 2\nINCLUDE: first.yml\n"
+
+      mock(File).read("first.yml") { first }
+      mock(File).read("second.yml") { second }
+
+      lambda {
+          YamlExtras.new("first.yml").integrate_includes
+        }.should raise_error(CyclicYamlExtrasInclude)
     end
   end
 end
